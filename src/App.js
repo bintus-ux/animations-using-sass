3749ef1eafce84387a39a6bec2bdc68409b85c9b
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-12-17 02:47:59 +0100

    'update'

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
 const App = () => {
   return (
     <>
-      <div class='card__collection clear-fix'>
-        <div class='cards cards--two'>
-          <img
-            src='https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d702cb99ca804bffcfa8820c46483264&auto=format&fit=crop&w=651&q=80'
-            class='img-responsive'
-            alt='Cards Image'
-          />
-          <span class='cards--two__rect'></span>
-          <span class='cards--two__tri'></span>
-          <p>Lucy Grace</p>
-          <ul class='cards__list'>
-            <li>
-              <i class='fab fa-facebook-f'></i>
-            </li>
-            <li>
-              <i class='fab fa-linkedin'></i>{' '}
-            </li>
-            <li>
-              <i class='fab fa-instagram'></i>
-            </li>
-            <li>
-              <i class='fab fa-linkedin-in'></i>
-            </li>
-          </ul>
+      <div class='card'>
+        <img
+          src='https://images.unsplash.com/photo-1524749292158-7540c2494485?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRldmVsb3BlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+          alt=''
+        />
+        <div class='card_content'>
+          <h2 class='card_title'>Lorem</h2>
+          <p class='card_description'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            porro dolores sapiente.
+          </p>
         </div>
-        <div class='cards cards--three'>
-          <img
-            src='https://images.unsplash.com/photo-1480408144303-d874c5e12201?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=575213599ae24b3dbdfd84be79425c50&auto=format&fit=crop&w=634&q=80'
-            class='img-responsive'
-            alt=''
-          />
-          <span class='cards--three__rect-1'>
-            <span class='shadow-1'></span>
-            <p>Chris Levnon</p>
-          </span>
-          <span class='cards--three__rect-2'>
-            <span class='shadow-2'></span>
-          </span>
-          <span class='cards--three__circle'></span>
-          <ul class='cards--three__list'>
-            <li>
-              <i class='fab fa-facebook-f'></i>
-            </li>
-            <li>
-              <i class='fab fa-twitter'></i>
-            </li>
-            <li>
-              <i class='fab fa-linkedin-in'></i>
-            </li>
-          </ul>
+      </div>
+
+      <div class='card card_dark'>
+        <img
+          src='https://media.istockphoto.com/photos/put-more-in-get-more-out-picture-id1291318636?b=1&k=20&m=1291318636&s=170667a&w=0&h=UvVIk7wwkN3X9OFm8gBlWWviV5vAjfrq2ejYP30JmnA='
+          alt=''
+        />
+        <div class='card_content'>
+          <h2 class='card_title'>Lorem</h2>
+          <p class='card_description'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque amet
+            obcaecati nihil.
+          </p>
         </div>
+      </div>
+      <div className='container'>
+        <button type='button' className='slide_in'>
+          Slide In
+        </button>
       </div>
     </>
   )
